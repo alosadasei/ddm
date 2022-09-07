30c4e43a51e1853565105648f21bde32439cfaa4
alosadasei <110858361+alosadasei@users.noreply.github.com>
2022-09-07 23:52:11 +0200

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,5 +1,7 @@
 package ddm.controllers;
 
+import java.util.Random;
+
 import ddm.objects.DungeonMaster;
 import ddm.objects.Square;
 
@@ -7,6 +9,7 @@ public class Game {
 	
 	private DungeonMaster dm0;
 	private DungeonMaster dm1;
+	private DungeonMaster player;
 	private Square[][] board;
 	
 	public Game() {}
@@ -15,5 +18,22 @@ public class Game {
 		dm0=new DungeonMaster(0,player1);
 		dm1=new DungeonMaster(1,player2);
 		board = new Square[13][19];
+		Random random = new Random();
+		if(random.nextInt(2)<1) {
+			player = dm0;
+		}
+		else {
+			player = dm1;
+		}
 	}
+	
+	public void changeTurn() {
+		if(player.getId()==0) {
+			player = dm1;
+		}
+		else {
+			player = dm0;
+		}
+	}
+	
 }
